313054591bed6551c1bd17194aeb0ca33008f801
kilelx <llx.kieran@gmail.com>
2022-11-16 08:55:30 +0100

    chaton

--- observer.js
+++ observer.js
@@ -5,12 +5,20 @@ let options = {
 }
 
 const displayRadius = (entries) => {
-    console.log(entries);
+
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            console.log(scrollY);
+            entry.target.style.borderRadius = `${1-(scrollY*0.1)}rem 1rem 0 0;`;
+        }
+    })
 }
 
 const osberver = new IntersectionObserver(displayRadius, options);
 
 
 sections.forEach(section => {
-    osberver.observe(section)
+    osberver.observe(section);
 })
+
+// clientY : hauteur de la souris lors d'un évènement
--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const body = document.querySelector('body');
+
 const title = document.querySelector("section h1");
 
 window.addEventListener("scroll", () => {
+    // body.style.transform = `translateY(${0+(scrollY*0.1)})`;
     title.style.transform = `translate(-50%, -${50+(scrollY)*0.1}%)`
 })
+
+window.addEventListener("click", (e) => {
+    console.log('clientX', e.clientY);
+    console.log('pageY', e.pageY);
+    console.log('screenY', e.screenY);
+    console.log('offsetY', e.offsetY);
+});
